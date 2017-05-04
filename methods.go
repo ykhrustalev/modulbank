f3d22c4a1417f5e706ffd49e1ed14006aef1f70d
Yuri Khrustalev <yuri.khrustalev@gmail.com>
2017-05-04 22:30:54 +0100

    add balance

--- methods.go
+++ methods.go
@@ -2,12 +2,14 @@ package modulbank
 
 import (
 	"fmt"
+	"io/ioutil"
+	"strconv"
 )
 
 // Requires
 // account-info
-func (client *Client) V1AccountInfo() ([]AccountInfo, error) {
-	logger := client.logger.WithField("method", "V1AccountInfo")
+func (client *Client) V1_AccountInfo() ([]AccountInfo, error) {
+	logger := client.logger.WithField("method", "V1_AccountInfo")
 
 	var result []AccountInfo
 	_, err := client.handleRequest(logger, "POST", "/v1/account-info", nil, &result)
@@ -20,12 +22,12 @@ func (client *Client) V1AccountInfo() ([]AccountInfo, error) {
 
 // Requires
 // operation-history
-func (client *Client) V1OperationHistory(account string, search *OperationHistorySearch) ([]Operation, error) {
-	logger := client.logger.WithField("method", "V1AccountInfo")
+func (client *Client) V1_OperationHistory(bankAccountId string, search *OperationHistorySearch) ([]Operation, error) {
+	logger := client.logger.WithField("method", "V1_OperationHistory")
 
 	var result []Operation
 
-	path := fmt.Sprintf("/v1/operation-history/%s", account)
+	path := fmt.Sprintf("/v1/operation-history/%s", bankAccountId)
 	if search == nil {
 		search = &OperationHistorySearch{}
 	}
@@ -36,4 +38,37 @@ func (client *Client) V1OperationHistory(account string, search *OperationHistor
 	}
 
 	return result, nil
+}
+
+// Requires
+// account-info
+func (client *Client) V1_AccountBalance(bankAccountId string) (result float64, err error) {
+	logger := client.logger.WithField("method", "V1_AccountBalance")
+
+	path := fmt.Sprintf("/v1/account-info/balance/%s", bankAccountId)
+
+	req, err := client.buildRequest(logger, "POST", path, nil)
+	if err != nil {
+		return
+	}
+
+	resp, err := client.doRequest(logger, req, 200)
+	defer client.closeBody(resp)
+	if err != nil {
+		return
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		logger.WithError(err).Error("failed to read body")
+		return
+	}
+
+	result, err = strconv.ParseFloat(string(body), 64)
+	if err != nil {
+		logger.WithError(err).Error("failed to parse value from body")
+		return
+	}
+
+	return
 }
